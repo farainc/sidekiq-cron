0f1d9292b3746cdee044d70a8c8cbb7e9efc91c9
josh.c <junyu.live@gmail.com>
2020-08-07 10:36:54 -0700

    Add redis conn.exists? method compatible.

--- lib/sidekiq/cron.rb
+++ lib/sidekiq/cron.rb
@@ -6,3 +6,15 @@ module Sidekiq
   module Cron
   end
 end
+
+if Redis::VERSION < '4.2'
+  module RedisCompatible
+    extend ActiveSupport::Concern
+
+    def exists?(key)
+      exists(key)
+    end
+  end
+
+  Redis.send(:include, RedisCompatible)
+end
